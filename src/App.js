70a7ddca2cb084ea6ec7426b48b59cd6d053d677
KAMRAN <kamranjaved1234@gmail.com>
2023-11-18 13:59:36 +0530

    URL changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import User from './pages/User';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<main>
+			<Navbar />
+			<Routes>
+				<Route exact path="/" element={<Hero />} />
+				<Route path="/users" element={<User />} />
+			</Routes>
+		</main>
+	);
 }
 
 export default App;
